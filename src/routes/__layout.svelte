865e7e1e14a007e9cb49de96ed24588002734794
jweilage <jackweilage@gmail.com>
2022-02-23 10:50:12 -0800

    create default layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,23 @@
-<slot />
+<script>
+    import SkipToLink from '$lib/components/SkipToLink'
+    import Header from '$lib/components/Header'
+    import Footer from '$lib/components/Footer'
+</script>
+
+<SkipToLink href="#main-content" />
+
+<div>
+    <Header />
+    <slot />
+    <Footer />
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-flow: column nowrap;
+
+        width: 100%;
+        height: 100%;
+    }
+</style>
