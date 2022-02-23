177333e652474a1fabbda39074999eed3c5e694a
jweilage <jackweilage@gmail.com>
2022-02-23 10:50:06 -0800

    create error route

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -15,21 +15,57 @@
     <title>Error {status}</title>
 </svelte:head>
 
-<div>
-    {#if online}
-        <h1>{status}</h1>
-        {#if error.message}
-            <p>{status}: {error.message}</p>
+<main id="main-content">
+    <div>
+        {#if online}
+            <h1>{status}</h1>
+            {#if error.message}
+                <p>{status}: {error.message}</p>
+            {:else}
+                <p>You found a {status} error!</p>
+            {/if}
+            {#if import.meta.env.DEV}
+                <pre>{error.stack}</pre>
+            {:else}
+                <p>If this error persists, please contact me with a description of how to reproduce the error.</p>
+            {/if}
         {:else}
-            <p>You found a {status} error!</p>
+            <h1>You're currently offline!</h1>
+            <p>Wait for an internet connection, then reload this page.</p>
         {/if}
-        {#if import.meta.env.DEV}
-            <pre>{error.stack}</pre>
-        {:else}
-            <p>If this error persists, please contact me with a description of how to reproduce the error.</p>
-        {/if}
-    {:else}
-        <h1>You're currently offline!</h1>
-        <p>Wait for an internet connection, then reload this page.</p>
-    {/if}
-</div>
+    </div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        padding: 2rem;
+    }
+    div {
+        flex-grow: 1;
+
+        padding: 1rem;
+
+        border-radius: 0.25rem;
+        border: 1px solid #ccc;
+
+        box-shadow: 0 0 0.5rem 0 rgba(0, 0, 0, 0.25);
+    }
+    h1 {
+        font-size: 2rem;
+
+        margin-bottom: 1rem;
+
+        border-bottom: 1px solid #ccc;
+    }
+    p {
+        margin-bottom: 0.5rem;
+    }
+    pre {
+        font-size: 0.8rem;
+        white-space: pre-wrap;
+        word-break: break-all;
+
+        margin-bottom: 0.5rem;
+    }
+</style>
