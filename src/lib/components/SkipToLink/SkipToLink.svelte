dee5f91b01fef322e69b58cc1ec32a49015f8549
jweilage <jackweilage@gmail.com>
2022-02-23 10:49:50 -0800

    create skiptolink component

---
+++ src/lib/components/SkipToLink/SkipToLink.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let href = '#main-content'
+</script>
+
+<a {href}>Skip To Main Content</a>
+
+<style>
+    a {
+        position: absolute;
+        left: 0;
+
+        margin: 0.5rem;
+        padding: 0.5rem;
+        
+        background: #fff;
+        
+        border-radius: 0.25rem;
+        border: 1px solid #ccc;
+
+        z-index: 999;
+        transform: translateY(calc(-100% - 1px - 0.5rem - 0.5rem));
+
+        transition: transform 0.5s;
+    }
+    a:focus {
+        transform: translateY(0);
+    }
+</style>
