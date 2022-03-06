05e06c62fca5e3af39dbabcc02d661dd04cd7357
jweilage <jackweilage@gmail.com>
2022-03-06 10:52:13 -0800

    make page more semantic, use better css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,9 @@
-<script lang="ts">
-    /*
-    let text: HTMLInputElement
-    let info: HTMLParagraphElement
-
-    let unlocked = false
-    const on_submit = () => {
-        fetch('/degen-unlock', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ password: text.value })
-        }).then(res => res.json()).then(res => {
-            ({ unlocked } = res)
-            if (res.unlocked) {
-                info.textContent = 'Unlocked!'
-            } else {
-                info.textContent = 'Incorrect Password'
-            }
-        })
-    }
-    */
-</script>
-
 <svelte:head>
     <title>Home - Jack Weilage</title>
 </svelte:head>
 
 <main id="main-content">
-    <div class="main">
+    <section>
         <h1>
             Hello! I'm
             <br>
@@ -40,48 +17,32 @@
             <a rel="external" target="_blank" href="https://www.typescriptlang.org/">TypeScript</a>.
             I enjoy taking ideas and turning them into reality.
         </p>
-    </div>
-    <!-- <div class="degen">
-        {#if unlocked}
-            <p>test</p>
-        {:else}
-            <form on:submit|preventDefault={on_submit}>
-                <label for="entry">What website is the "degen website"</label>
-                <input bind:this={text} type="text" name="entry" placeholder="enter the url (11 characters)" autocomplete="off" required minlength="11" maxlength="11">
-                <input type="submit" placeholder="Submit" required>
-                <p bind:this={info}></p>
-            </form>
-        {/if}
-    </div> -->
+    </section>
 </main>
 
 <style>
     main {
+        box-sizing: border-box;
+
         width: 100%;
         height: 100%;
+        max-height: 100dvh;
         max-height: 100vh;
-
-        /* overflow-y: auto;
-        scroll-snap-type: y proximity; */
-    }
-    div.main {
-        box-sizing: border-box;
 
         display: flex;
         flex-flow: column wrap;
         align-content: center;
         justify-content: center;
 
-        width: 100%;
-        height: 100vh;
-
         padding: 1rem;
 
-        /* scroll-snap-align: start; */
+        /* overflow-y: auto;
+        scroll-snap-type: y proximity; */
     }
     h1 {
         font-size: 1.8rem;
         font-weight: bold;
+
         margin-bottom: 0.25rem;
     }
     span#name {
@@ -89,13 +50,8 @@
         color: #00B84D;
     }
     p {
-        line-height: 1.1;
         max-width: 25rem;
-    }
-    /* div.degen {
-        width: 100%;
-        min-height: 100vh;
 
-        scroll-snap-align: start;
-    } */
+        line-height: 1.1;
+    }
 </style>
