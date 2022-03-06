5c902a11fbc0143c731f1b6474d353c12fa50a71
jweilage <jackweilage@gmail.com>
2022-03-05 19:20:15 -0800

    remove extrainious components

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,6 @@
 <script>
     import SkipToLink from '$lib/components/SkipToLink'
-    import Header from '$lib/components/Header'
-    import Footer from '$lib/components/Footer'
 </script>
 
 <SkipToLink href="#main-content" />
-
-<div>
-    <Header />
-    <slot />
-    <Footer />
-</div>
-
-<style>
-    div {
-        display: flex;
-        flex-flow: column nowrap;
-
-        width: 100%;
-        height: 100%;
-    }
-</style>
+<slot />
