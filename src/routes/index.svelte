864e5171366aaac5417a5819592b3377ad3e94be
jweilage <jackweilage@gmail.com>
2022-03-08 20:10:14 -0800

    better semantics, fancy animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,34 @@
+<script>
+    import { onMount } from 'svelte'
+    import { fly, fade } from 'svelte/transition'
+
+    /** @see [original function](https://easings.net/#easeOutBack) */
+    function easeOutBack(t) {
+        const c1 = 1.70158;
+        const c3 = c1 + 1;
+
+        return 1 + c3 * Math.pow(t - 1, 3) + c1 * Math.pow(t - 1, 2);
+    }
+
+    let mounted = false
+    let reducedMotion = false
+    $: transition = reducedMotion ? (node, options) => fade(node, {
+        delay: options?.delay, duration: options?.duration, easing: options?.easing
+    }) : fly
+    onMount(() => {
+        mounted = true
+        reducedMotion = window.matchMedia('(prefers-reduced-motion)').matches
+    })
+</script>
 <svelte:head>
     <title>Home - Jack Weilage</title>
 </svelte:head>
 
+{#if mounted}
 <main id="main-content">
-    <section>
-        <h1>
-            Hello! I'm<br>
-            <span id="name">Jack&nbsp;Weilage</span>
-        </h1>
+    <section in:transition={{ y: -100, duration: 1000, delay: 250 }}>
+        <h2>Hello! I'm</h2>
+        <h1 in:transition={{ x: -100, duration: 500, delay: 750, easing: easeOutBack }}>Jack&nbsp;Weilage</h1>
         <p>
             I'm a web developer currently using
             <a rel="external" target="_blank" href="https://svelte.dev/">Svelte</a>, 
@@ -17,13 +38,14 @@
             I enjoy taking ideas and turning them into reality.
         </p>
     </section>
-    <aside>
+    <aside in:transition={{ y: -50, duration: 500, delay: 1100 }}>
         <p>Contact me on:</p>
         <address>
             <a href="mailto:jack@weilage.dev">Email (jack@weilage.dev)</a>
         </address>
     </aside>
 </main>
+{/if}
 
 <style>
     main {
@@ -46,19 +68,34 @@
         bottom: 1rem;
         right: 1rem;
     }
-    h1 {
+    h2 {
+        font-family: 'Oxygen', sans-serif;
         font-size: 1.8rem;
         font-weight: bold;
+    }
+    h1 {
+        font-family: 'Fira Sans Condensed', sans-serif;
+        font-size: 4rem;
+        font-weight: bold;
+
+        color: #00B84D;
 
         margin-bottom: 0.25rem;
-    }
-    span#name {
-        font-size: 3rem;
-        color: #00B84D;
     }
     p {
         line-height: 1.1;
 
         max-width: 25rem;
+    }
+    @media (max-width: 600px) {
+        h2 {
+            font-size: 1.5rem;
+        }
+        h1 {
+            font-size: 3rem;
+        }
+        p {
+            max-width: 20rem;
+        }
     }
 </style>
