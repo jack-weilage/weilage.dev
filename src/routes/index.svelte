0cbcbcc7a14cdd393c1997b704495f2c84b13b51
jweilage <jackweilage@gmail.com>
2022-03-16 13:52:10 -0700

    use new seo and fancier text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,14 @@
-<svelte:head>
-    <title>Home - Jack Weilage</title>
-    <link rel="canonical" href="https://weilage.dev">
-</svelte:head>
+<script>
+    import SEO from '$lib/components/SEO'
+    import GradientText from '$lib/components/GradientText'
+</script>
+
+<SEO 
+    title="Home - Jack Weilage"
+    description="weilage.dev is a personal website for Jack Weilage, a web developer and designer."
+    keywords={[ 'weilage', 'jack weilage', 'weilage.dev', 'weilage dev', 'portfolio', 'jack weilage portfolio' ]}
+    canonical="https://weilage.dev"
+/>
 
 <main id="main-content">
     <section>
@@ -12,7 +19,7 @@
             <a rel="external" target="_blank" href="https://svelte.dev/">Svelte</a>&nbsp;/&nbsp;<a rel="external" target="_blank" href="https://kit.svelte.dev/">SvelteKit</a>,
             <a rel="external" target="_blank" href="https://nodejs.org/">Node.js</a> and 
             <a rel="external" target="_blank" href="https://www.typescriptlang.org/">TypeScript</a>
-            to build highly accessible and fully semantic websites.
+            to build highly accessible and <GradientText --fill="linear-gradient(to right, red, orange, #f0f)">fully semantic</GradientText> websites.
         </p>
     </section>
     <aside>
@@ -41,12 +48,17 @@
 
         padding: 1rem;
     }
+    section {
+        padding: 1rem;
+    }
     aside {
         line-height: 1.1;
 
         position: absolute;
-        bottom: 1rem;
-        right: 1rem;
+        bottom: 0;
+        right: 0;
+        
+        margin: 1rem;
     }
     h1 {
         /* force style to italic and bold while the font is loading */
@@ -64,9 +76,7 @@
         font-weight: bold;
     }
     h3 {
-        /* font-family: 'Oxygen', sans-serif; */
         font-size: 1.4rem;
-        /* font-weight: bold; */
     }
     p {
         line-height: 1.1;
