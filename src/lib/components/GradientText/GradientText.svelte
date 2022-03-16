3b9c734a8891a0a122a026b29fa8d7e703dede8b
jweilage <jackweilage@gmail.com>
2022-03-16 13:49:57 -0700

    create a gradient text element

---
+++ src/lib/components/GradientText/GradientText.svelte
@@ -0,0 +1,13 @@
+<span><slot /></span>
+
+<style>
+    span {
+        /* text shouldn't wrap with a gradient */
+        white-space: nowrap;
+
+        background: var(--fill, linear-gradient(to right, red 0%, purple 25%, orange 100%));
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+</style>
