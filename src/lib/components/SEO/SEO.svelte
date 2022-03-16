deb9f6e892a8b27649e465e43b6c3530ca17eb43
jweilage <jackweilage@gmail.com>
2022-03-16 13:49:23 -0700

    create a head meta manager

---
+++ src/lib/components/SEO/SEO.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let title: string, description: string
+    export let keywords: string[]
+    export let canonical: string
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="description" content={description}>
+    <meta name="keywords" content={keywords.join(',')}>
+    <link rel="canonical" href={canonical}>
+</svelte:head>
