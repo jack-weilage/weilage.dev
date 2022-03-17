671447720dfbe28e28992816de8f50c5a1288c17
jweilage <jackweilage@gmail.com>
2022-03-16 19:04:27 -0700

    update default gradient, animate smoothly

--- src/lib/components/GradientText/GradientText.svelte
+++ src/lib/components/GradientText/GradientText.svelte
@@ -5,9 +5,21 @@
         /* text shouldn't wrap with a gradient */
         white-space: nowrap;
 
-        background: var(--fill, linear-gradient(to right, red 0%, purple 25%, orange 100%));
+        background: var(--fill, linear-gradient(to right, red 0%, orange 25%, orange 50%, red 100%));
         background-clip: text;
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
+
+        background-size: 200% 100%;
+        
+        animation: move-background 2.5s linear infinite;
+    }
+    @keyframes move-background {
+        from {
+            background-position: 100% 0;
+        }
+        to {
+            background-position: -100% 0;
+        }
     }
 </style>
