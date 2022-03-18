e8cf8d3c9f44bb9630d739a6f70e116da914aeda
jweilage <jackweilage@gmail.com>
2022-03-17 22:01:27 -0700

    create a component with a fancy underline

---
+++ src/routes/_contact.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Wrapper from '$lib/components/Wrapper'
+
+    export let type
+    export let href
+    export let text
+
+    let typeWidth = 0
+    let textWidth = 0
+</script>
+
+<Wrapper --type="{typeWidth}px" --text="{textWidth}px">
+    <div>
+        <span bind:clientWidth={typeWidth}>{type}:</span>
+        <a {href} bind:clientWidth={textWidth}>{text}</a>
+        <div class="underline" />
+    </div>
+</Wrapper>
+
+<style>
+    div {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+    }
+    div.underline {
+        width: calc(var(--type) * 1.1);
+        height: 0.5px;
+
+        margin: 0.25rem calc(100% - var(--type) * 1.1) 0.25rem 0;
+        background-color: #ccc;
+
+        transition: width 0.5s, margin 0.5s;
+    }
+    div:hover div.underline {
+        width: calc(var(--text) * 1.1);
+        margin: 0.25rem 0 0.25rem calc(100% - var(--text) * 1.1);
+    }
+    span {
+        display: inline-block;
+        margin-right: auto;
+    }
+    a {
+        display: inline-block;
+    }
+    a:hover {
+        text-decoration: none;
+    }
+</style>
