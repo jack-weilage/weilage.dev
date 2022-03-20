bf7df08fbecdb2a809181956c82069fe7b8d2321
jweilage <jackweilage@gmail.com>
2022-03-19 20:18:17 -0700

    create a component to display a "real" loading bar

---
+++ src/lib/components/LoadingIndicator/LoadingIndicator.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from 'svelte'
+    
+    let width = 0
+    onMount(() => {
+        const update = () => {
+            width += 5
+
+            if (width < 80) setTimeout(update, 10 * width)
+        }
+        setTimeout(update, 250)
+    })
+</script>
+
+{#if width > 0}
+<div style:width="{width}%" />
+{/if}
+
+<style>
+    div {
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        height: 2px;
+
+        background-color: var(--theme-green);
+
+        z-index: 999;
+
+        transition: width 0.5s ease-in-out;
+    }
+</style>
