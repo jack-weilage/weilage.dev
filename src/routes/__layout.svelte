e7a56a6009e9128afc8648bd9355faf87fca62f7
jweilage <jackweilage@gmail.com>
2022-03-19 20:18:41 -0700

    use "real" loading indicator

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,13 @@
 <script>
+    import LoadingIndicator from '$lib/components/LoadingIndicator'
     import SkipToLink from '$lib/components/SkipToLink'
+
+    import { navigating } from '$app/stores'
 </script>
+
+{#if $navigating}
+    <LoadingIndicator />
+{/if}
 
 <SkipToLink />
 <slot />
