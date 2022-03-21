3bea82d83b010e04405448ea9ce22babdb78a35d
Jack Weilage <jack@weilage.dev>
2022-03-21 16:48:56 -0700

    i don't know if precompress is useful, so disable

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,9 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		trailingSlash: 'always',
-		adapter: adapter({ precompress: true })
+		adapter: adapter()
 	}
 }
 
