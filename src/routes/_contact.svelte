edf64f9eb1887669fad48a6d1a5bd106b62a86e9
Jack Weilage <jack@weilage.dev>
2022-03-21 11:44:17 -0700

    fix css vars not having a px value

--- src/routes/_contact.svelte
+++ src/routes/_contact.svelte
@@ -8,13 +8,15 @@
     let typeWidth = 10
     let textWidth = 10
 
-    $: varsList = { typeWidth, textWidth }
+    $: varsList = { type: `${typeWidth}px`, text: `${textWidth}px` }
 </script>
 
-<div use:vars={varsList}>
+<!-- this could be refactored with slots -->
+<!-- how can we get the width of a slot? -->
+<div>
     <span bind:clientWidth={typeWidth}>{type}:</span>
     <a rel="external" target="_blank" {href} bind:clientWidth={textWidth}>{text}</a>
-    <div class="underline" />
+    <div class="underline" use:vars={varsList} />
 </div>
 
 <style>
@@ -27,17 +29,17 @@
         overflow: hidden;
     }
     div.underline {
-        width: calc(var(--typeWidth) + 0.45rem);
+        width: calc(var(--type) + 0.45rem);
         height: 0.5px;
 
-        margin: 0.1rem calc(100% - var(--typeWidth) - 0.45rem) 0.1rem 0;
+        margin: 0.1rem calc(100% - var(--type) - 0.45rem) 0.1rem 0;
         background-color: #ccc;
 
         transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
     }
     div:hover div.underline {
-        width: calc(var(--textWidth) + 0.45rem);
-        margin: 0.1rem 0 0.1rem calc(100% - var(--textWidth) - 0.45rem);
+        width: calc(var(--text) + 0.45rem);
+        margin: 0.1rem 0 0.1rem calc(100% - var(--text) - 0.45rem);
         background-color: var(--theme-anchor);
 
         /* delay going to right */
