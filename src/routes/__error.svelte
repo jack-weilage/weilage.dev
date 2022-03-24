ab0a35557ef7087813676b87e3071171109b8dad
Jack Weilage <jack@weilage.dev>
2022-03-24 14:22:45 -0700

    use SvEO to prevent indexing of error pages

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -7,33 +7,37 @@
     }
 </script>
 <script lang="ts">
+    import SvEO from '$lib/components/SvEO'
+
     export let error: { stack: string, name: string, message: string}, status: number
     const online = typeof navigator !== 'undefined' ? navigator.onLine : true
 </script>
 
-<svelte:head>
-    <title>Error {status}</title>
-</svelte:head>
+<SvEO
+    title="Error {status}"
+    description="An error occurred while loading this page."
+    keywords={[]}
+    canonical=""
+    noindex noarchive
+/>
 
 <main id="main-content">
-    <div>
-        {#if online}
-            <h1>{status}</h1>
-            {#if error.message}
-                <p>{status}: {error.message}</p>
-            {:else}
-                <p>You found a {status} error!</p>
-            {/if}
-            {#if import.meta.env.DEV}
-                <pre>{error.stack}</pre>
-            {:else}
-                <p>If this error persists when it seems like it really shouldn't, please contact me at <a href="mailto:jack@weilage.dev?subject=Recurring Error {status}: {error.message}">jack@weilage.dev</a> with a description of how to reproduce the error.</p>
-            {/if}
+    {#if online}
+        <h1>{status}</h1>
+        {#if error.message}
+            <p>{status}: {error.message}</p>
         {:else}
-            <h1>You're currently offline!</h1>
-            <p>Wait for an internet connection, then reload this page.</p>
+            <p>You found a {status} error!</p>
         {/if}
-    </div>
+        {#if import.meta.env.DEV}
+            <pre>{error.stack}</pre>
+        {:else}
+            <p>If this error persists when it seems like it really shouldn't, please contact me at <a href="mailto:jack@weilage.dev?subject=Recurring Error {status}: {error.message}">jack@weilage.dev</a> with a description of how to reproduce the error.</p>
+        {/if}
+    {:else}
+        <h1>You're currently offline!</h1>
+        <p>Wait for an internet connection, then reload this page.</p>
+    {/if}
 </main>
 
 <style>
@@ -41,22 +45,12 @@
         display: flex;
         padding: 2rem;
     }
-    div {
-        flex-grow: 1;
-
-        padding: 1rem;
-
-        border-radius: 0.25rem;
-        border: 1px solid #ccc;
-
-        box-shadow: 0 0 0.5rem 0 rgba(0, 0, 0, 0.25);
-    }
     h1 {
         font-size: 2rem;
 
         margin-bottom: 1rem;
 
-        border-bottom: 1px solid #ccc;
+        border-bottom: 1px solid var(--theme-text);
     }
     p {
         margin-bottom: 0.5rem;
