4a37f9281348038e82c5c2126391e3aeff36e37c
Jack Weilage <jack@weilage.dev>
2022-03-28 14:33:47 -0700

    prevent errors if feather is undefined

--- src/lib/components/FeatherIcon/FeatherIcon.svelte
+++ src/lib/components/FeatherIcon/FeatherIcon.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <!-- TODO: i highly doubt that there would be a way to poison this html code, but it should still be sanitized -->
-{#if feather.icons[name]}
+{#if feather?.icons[name]}
     {@html feather.icons[name].toSvg({ width, height })}
 {:else}
     <div style:width style:height />
