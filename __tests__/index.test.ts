29a2e533c5a45e2cc3cc55ec448e9533cad36f1f
Jack Weilage <jack@weilage.dev>
2022-04-04 19:03:53 -0700

    first pass on tests, CI coming later

---
+++ __tests__/index.test.ts
@@ -0,0 +1,47 @@
+// const { exec } = require('child_process')
+
+// let server = null
+
+// describe('dev', () => {
+//     beforeAll(async () => await new Promise(resolve => {
+//         server = exec('npm run build && npm run preview')
+//         server.stdout.on('data', data => {
+//             if (data.includes('localhost')) resolve(1)
+//             if (data.includes('is occupied')) throw new Error('Port is occupied')
+//         })
+//     }))
+
+//     describe('/', () => {
+//         beforeAll(async () => await page.goto('http://localhost:3000/'))
+
+//         it('should have a correct title', async () => {
+//             expect(await page.title()).toBe('Home - Jack Weilage')
+//         })
+//     })
+
+//     afterAll(() => server.kill())
+// })
+describe('prod', () => {
+    describe('/', () => {
+        beforeAll(async () => await page.goto('https://weilage.dev/'))
+
+        it('should have a correct title', async () => {
+            expect(await page.title()).toBe('Home - Jack Weilage')
+        })
+        it('should have a skip link', async () => {
+            expect(await page.$('a[href="#main-content"]')).toBeTruthy()
+        })
+        it('should have a main content', async () => {
+            expect(await page.$('main#main-content')).toBeTruthy()
+        })
+        it('should have only one h1', async () => {
+            expect(await page.$$('h1')).toHaveLength(1)
+        })
+        it('should have a h1 with the correct text', async () => {
+            expect(await page.$eval('h1', el => el.innerHTML)).toBe('Jack&nbsp;Weilage')
+        })
+        it('should have an aside in the main content', async () => {
+            expect(await page.$('main > aside')).toBeTruthy()   
+        })
+    })
+})
