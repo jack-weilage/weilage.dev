ee89273a4eba2ab52d588a070cd051a55e5fa4e5
Jack Weilage <jack@weilage.dev>
2022-04-05 17:39:49 -0700

    remove gradient text/nbsp, animate contact text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,62 @@
 <script>
+    import { slide }     from 'svelte/transition'
     import SvEO         from '$lib/components/SvEO'
     import Link         from '$lib/components/Link'
-    import GradientText from '$lib/components/GradientText'
 
     import Email20      from 'carbon-icons-svelte/lib/Email20'
     import LogoGithub20 from 'carbon-icons-svelte/lib/LogoGithub20'
 
     import Contact from './_contact.svelte'
+
+    let contactText = 'Contact me'
 </script>
 
 <SvEO
     title="Home - Jack Weilage"
     description="Welcome to weilage.dev, a personal website for Jack Weilage, a web developer and designer."
     author="Jack Weilage"
-    keywords={[ 'weilage', 'jack weilage', 'jack weilage dev', 'jack weilage developer', 'portfolio', 'developer', 'developer portfolio', 'jack weilage portfolio' ]}
+    keywords={[
+        'weilage', 'jack weilage', 'jack weilage dev', 'jack weilage developer',
+        'portfolio', 'developer', 'developer portfolio', 'jack weilage portfolio' 
+    ]}
     canonical="https://weilage.dev/"
 />
-
 <main id="main-content">
     <div>
         <h2>Hello! I'm</h2>
-        <h1>Jack&nbsp;Weilage</h1>
+        <h1>Jack Weilage</h1>
         <p class="description">
             I'm a web developer currently using
             <Link href="https://svelte.dev">Svelte</Link>&nbsp;/&nbsp;<Link href="https://kit.svelte.dev">SvelteKit</Link>,
             <Link href="https://nodejs.org">Node.js</Link> and
-            <Link href="https://www.typescriptlang.org/">TypeScript</Link>
-            to build highly&nbsp;accessible and <GradientText>fully semantic</GradientText> websites.
+            <Link href="https://www.typescriptlang.org">TypeScript</Link>
+            to build highly accessible and fully semantic websites.
         </p>
     </div>
     <aside>
-        <p class="contacts">Contact me</p>
-        <address>
+        {#key contactText}
+            <!-- transitions currently create a memory leak (svelte pls fix) -->
+            <p class="contacts" transition:slide={{ delay: 100 }}>{contactText}</p>
+        {/key}
+        <address
+            on:mouseleave={() => contactText = 'Contact me'}
+            on:blur={      () => contactText = 'Contact me'}
+        >
             <ul>
-                <li>
+                <li
+                    on:mouseover={() => contactText = 'Email me'}
+                    on:focus={    () => contactText = 'Email me'}
+                >
                     <Contact>
                         <Email20 slot="icon" />
                         <span slot="type" class="contact-type">Email</span>
                         <a slot="link" class="contact-link" rel="external" target="_blank" title="Email me" href="mailto:jack@weilage.dev">jack@weilage.dev</a>
                     </Contact>
                 </li>
-                <li>
+                <li 
+                    on:mouseover={() => contactText = 'Read my code'}
+                    on:focus={    () => contactText = 'Read my code'}
+                >
                     <Contact>
                         <LogoGithub20 slot="icon" />
                         <span slot="type" class="contact-type">GitHub</span>
