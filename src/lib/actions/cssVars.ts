d39594122ae8b4751a5734dc96f439d47c636966
Jack Weilage <jack@weilage.dev>
2022-04-12 13:10:30 -0700

    rewrite for readability

--- src/lib/actions/cssVars.ts
+++ src/lib/actions/cssVars.ts
@@ -1,17 +1,36 @@
-const updateStyles = (node: HTMLElement, styles: Record<string, string | number>) => {
+type Styles = Record<string, string>
+
+/** update vars on a node */
+const updateStyles = (node: HTMLElement, styles: Styles) => {
+    const updated_keys = []
     // for every key and value of the styles object, set a var on the node
     for (const [key, value] of Object.entries(styles)) {
         node.style.setProperty(`--${key}`, value.toString())
+        updated_keys.push(key)
     }
+
+    return updated_keys
 }
-export default (node: HTMLElement, styles: Record<string, string | number>) => {
+/** remove vars from a node */
+const removeStyles = (node: HTMLElement, styles: string[]) => styles.forEach(key => node.style.removeProperty(`--${key}`))
+
+
+export default (node: HTMLElement, styles: Styles) => {
+    // update the variables on first render
     updateStyles(node, styles)
 
     return {
-        update(new_styles: Record<string, string | number>) {
-            updateStyles(node, new_styles)
-            // for each key in the initial var list, if it's been removed from the new list, remove it from the style list
-            Object.keys(new_styles).forEach(key => !styles.hasOwnProperty(key) && node.style.removeProperty(`--${key}`))
+        update(updated_styles: Styles) {
+            // update the variables
+            const updated_keys = updateStyles(node, updated_styles)
+
+            const old_keys = Object.keys(styles)
+            // remove any variables that no longer exist
+            removeStyles(node, old_keys.filter(key => !updated_keys.includes(key)))
+            // remove any variables that have no value
+            removeStyles(node, old_keys.filter(key => !updated_styles[key]))
+
+            styles = updated_styles
         }
     }
 }
