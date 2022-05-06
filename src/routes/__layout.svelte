f44800663d09f20d2b1aac0888c109ee45cfba1c
Jack Weilage <jack@weilage.dev>
2022-05-05 17:21:47 -0700

    Replace auto-sizing with CSS

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,24 +5,10 @@
     import { LoadingIndicator } from '$lib/components'
 
     import { navigating } from '$app/stores'
-    import { onMount } from 'svelte'
-
-    let height
-    let mounted = false
-
-    onMount(() => mounted = true)
-
-    // Reactive functions cannot be used in onMount, so we use a flag to indicate that the component is mounted.
-    $: if (mounted)
-    {
-        document.body.style.setProperty('--auto-height', `${height}px`)
-    }
 </script>
 
 {#if $navigating && $navigating.to}
     <LoadingIndicator />
 {/if}
-
-<svelte:window bind:innerHeight={height} />
 
 <slot />
