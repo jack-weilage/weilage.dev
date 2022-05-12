ad9cba46900fc6ce0fd22ff74b79e47a79a78e34
Jack Weilage <jack@weilage.dev>
2022-05-12 13:58:26 -0700

    Properly set Twitter and OpenGraph SvEO defaults

--- src/lib/components/SvEO.svelte
+++ src/lib/components/SvEO.svelte
@@ -17,17 +17,29 @@
     /** Should search engines not archive this page. */
     export let noarchive = false
     
-    $: robots = [ noindex && 'noindex', nofollow && 'nofollow', noarchive && 'noarchive', ].filter(Boolean)
+    $: robots = [ 
+        noindex && 'noindex', 
+        nofollow && 'nofollow', 
+        noarchive && 'noarchive' 
+    ].filter(Boolean).join()
 
     /** Opengraph data (for social media). */
     export let opengraph: {
         title?: string
         description?: string
         url?: string
+        site_name?: string
         type?: string
-    } | false = {}
-    // if opengraph is false, don't output anything
-    //// opengraph = opengraph !== false ? defaults(opengraph, { title, description, url: canonical, type: 'website' }) : {}
+    } | false = false
+    
+    $: if (opengraph)
+    {
+        // Set Opengraph defaults
+        opengraph = {
+            ...{ title, description, url: canonical, type: 'website' },
+            ...opengraph,
+        }
+    }
     
     /** Twitter card data. */
     export let twitter: {
@@ -36,18 +48,26 @@
         creator?: string
         description?: string
         title?: string
-    } | false = {}
-    //// twitter = twitter !== false ? defaults(twitter, { card: 'summary', site: null, creator: null, description, title }) : {}
+    } | false = false
+
+    $: if (twitter)
+    {
+        // Set Twitter defaults
+        twitter = {
+            ...{ card: 'summary', description, title },
+            ...twitter,
+        }
+    }
 </script>
 
 <svelte:head>
     <title>{title}</title>
     <meta name="description" content={description}>
-    <meta name="keywords"    content={keywords.join(',')}>
+    <meta name="keywords"    content={keywords.join()}>
     <link rel="canonical"    href={canonical}>
 
-    {#if author}<meta name="author" content={author}>          {/if}
-    {#if robots}<meta name="robots" content={robots.join(',')}>{/if}
+    {#if author}<meta name="author" content={author}>{/if}
+    {#if robots}<meta name="robots" content={robots}>{/if}
 
     {#each Object.entries(opengraph) as [key, value]}
         {#if value}<meta property="og:{key}" content={value}>{/if}
