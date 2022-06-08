3a30279bfe13f97be3331c2ed82ec794863361c1
Jack Weilage <jack@weilage.dev>
2022-06-08 13:22:17 -0700

    Reference new components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,6 @@
-export { default as LoadingIndicator }  from './LoadingIndicator.svelte'
+export { default as Copy }              from './Copy.svelte'
+export { default as Input }             from './Input.svelte'
 export { default as Link }              from './Link.svelte'
+export { default as LoadingIndicator }  from './LoadingIndicator.svelte'
 export { default as SkipToLink }        from './SkipToLink.svelte'
 export { default as SvEO }              from './SvEO.svelte'
