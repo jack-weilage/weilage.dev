c349c2cfa1f0ff1b5f3f4c765edc7dd7dae1f70b
Jack Weilage <jack@weilage.dev>
2022-06-14 16:28:36 -0700

    Optimize sitemap creation

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -1,17 +1,16 @@
+/**
+ * TODO: Use `svelte.config.js` to adjust the sitemap.
+ */
 import type { RequestHandlerOutput } from '@sveltejs/kit/types'
 
 const url = 'https://weilage.dev'
 export async function get(): Promise<RequestHandlerOutput>
 {
     const urls = Object.keys(import.meta.glob('./**/*.svelte'))
-        // Ignore the leading dot
-        .map(file => file.substring(1))
         // Ignore any files starting with an underscore
         .filter(file => !file.includes('/_'))
-        // Convert to proper URLs
-        .map(file => url + file.replace('index.svelte', '').replace('.svelte', '/'))
-        // Create XML
-        .map(file => `<url><loc>${file}</loc><changefreq>daily</changefreq><priority>1</priority></url>`)
+        // Convert to proper URLs and assemble XML (could be done with multiple `.map` calls)
+        .map(file => `<url><loc>https://weilage.dev${file.substring(1).replace('/index.svelte', '/').replace('.svelte', '/')}</loc><changefreq>daily</changefreq><priority>1</priority></url>`)
         .join('')
 
     const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`
