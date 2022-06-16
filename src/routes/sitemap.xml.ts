b6ca8e15c0abf1b3faf8c3bd22b13820cd996ac8
Jack Weilage <jack@weilage.dev>
2022-06-16 14:11:30 -0700

    Use a function to construct the url XML (readable)

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -1,6 +1,8 @@
 /**
  * TODO: Use `svelte.config.js` to adjust the sitemap.
  */
+const create_xml = (url: string) => `<url><loc>https://weilage.dev${url}</loc><changefreq>daily</changefreq><priority>1</priority></url>`
+
 import type { RequestHandler } from '@sveltejs/kit/types'
 export const get: RequestHandler = function()
 {
@@ -8,18 +10,18 @@ export const get: RequestHandler = function()
         // Ignore any files starting with an underscore
         .filter(file => !file.includes('/_'))
         // Convert to proper URLs and assemble XML (could be done with multiple `.map` calls)
-        .map(file => `<url><loc>https://weilage.dev${file.substring(1).replace('/index.svelte', '/').replace('.svelte', '/')}</loc><changefreq>daily</changefreq><priority>1</priority></url>`)
+        .map(file => create_xml(file.substring(1).replace('/index.svelte', '/').replace('.svelte', '/')))
         .join('')
-
-    const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`
+    
+    const body = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`
 
     return {
         status: 200,
         headers: {
             'Cache-Control': 'no-cache',
-            'Content-Length': sitemap.length,
+            'Content-Length': body.length,
             'Content-Type': 'application/xml'
         },
-        body: `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`
+        body
     }
 }
