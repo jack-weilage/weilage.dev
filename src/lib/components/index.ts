e812293a0ace93a6a44a9a46406b9f62f9f19985
Jack Weilage <jack@weilage.dev>
2022-06-20 13:22:41 -0700

    Rename `Input` to `LabelledInput`

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,6 @@
 export { default as ColorPicker }       from './ColorPicker.svelte'
 export { default as Copy }              from './Copy.svelte'
-export { default as Input }             from './Input.svelte'
+export { default as LabelledInput }     from './LabelledInput.svelte'
 export { default as Link }              from './Link.svelte'
 export { default as LoadingIndicator }  from './LoadingIndicator.svelte'
 export { default as SkipToLink }        from './SkipToLink.svelte'
