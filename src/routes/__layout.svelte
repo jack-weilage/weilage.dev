d4c6e866ccf8a3a835097d594e7929ca518d229e
Jack Weilage <jack@weilage.dev>
2022-06-20 13:42:05 -0700

    Add a todo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,7 @@
+<!-- 
+TODO: Test whether `LoadingIndicator` works.
+ -->
 <script>
-    /**
-     * I attempted to use `import.meta.globEager` to import all `.css` files automatically, but CSS requires
-     * dependencies to be correctly ordered, which seems to not be possible with `import.meta.globEager`.
-     * 
-     * I also attempted to use `@import` in the `.css` files, but this created a FOUT each reload.
-     * 
-     * Just manually importing each file seems to work best.
-    */
     import 'normalize.css'
     import '$lib/css/vars.css'
     import '$lib/css/global.css'
