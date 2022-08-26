e0361e214d421f6dbaf500c12ab6da89662aa3e3
Jack Weilage <jack@weilage.dev>
2022-08-26 12:50:27 -0700

    Remove `<SkipToLink />`, clean up layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import '!css/vars.css'
     import '!css/global.css'
 
-    import { LoadingIndicator, SkipToLink, Link } from '!components'
+    import { LoadingIndicator } from '!components'
     import { navigating, page } from '$app/stores'
 </script>
 
@@ -12,8 +12,11 @@
 {/if}
 {#if $page.url.pathname !== '/' && $page.url.pathname !== '/rewrite/'}
     <header>
-        <SkipToLink href="#main-content" />
-        <Link href="/" icon={false}>Home</Link>
+        <a href="#main-content" class="skip-to-main">Skip To Main Content</a>
+        <nav>
+            <a href="/" sveltekit:prefetch>Home</a>
+            <!-- <a href="/blog/" sveltekit:prefetch>Blog</a> -->
+        </nav>
     </header>
 {/if}
 
@@ -30,7 +33,36 @@
     header {
         @include flex($wrap: wrap, $align: flex-start);
 
-        padding: 1rem;
         border-bottom: 1px solid var(--theme-border);
+        
+        a.skip-to-main {
+            position: absolute;
+            left: 0;
+
+            margin: 0.5rem;
+            padding: 0.5rem;
+            
+            color: #000;
+            background-color: #fff;
+            
+            border-radius: 0.25rem;
+            border: 1px solid #ccc;
+
+            z-index: 999;
+            transform: translateX(-200%);
+
+            transition: transform 0.25s ease-in-out;
+        }
+        a.skip-to-main:focus {
+            transform: translateX(0);
+        }
+        
+        nav {
+            @include flex;
+
+            a {
+                padding: 1rem;
+            }
+        }
     }
 </style>
