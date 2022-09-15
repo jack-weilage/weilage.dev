8594ac7c468d97e96901308afd62a5e3d7a4a9f7
Jack Weilage <jack@weilage.dev>
2022-09-15 15:55:15 -0700

    No JS by default

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,3 @@
-export const csr = false
-export const prerender = true
-
 // Sitemap configuration
 import type { SitemapConfig } from '!types'
 export const sitemap: SitemapConfig = { enabled: true, priority: 1, changefreq: 'daily' }
