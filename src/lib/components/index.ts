0f46b3cef24daa493de51fec8fe4e8ef34c5d2e8
Jack Weilage <jack@weilage.dev>
2022-09-15 15:53:12 -0700

    Remove unused file

--- src/lib/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Figure }            from './Figure.svelte'
-export { default as LabelledInput }     from './LabelledInput.svelte'
-export { default as Link }              from './Link.svelte'
-export { default as SvEO }              from './SvEO.svelte'
