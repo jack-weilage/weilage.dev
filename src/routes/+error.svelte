df0490535186e686f95098045353a8d02612d24a
Jack Weilage <jack@weilage.dev>
2022-09-15 14:05:05 -0700

    Eh, don't really need this, `svelte-kit check` doesn't like it

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -21,15 +21,11 @@
         {:else}
             <p>You found a {$page.status} error!</p>
         {/if}
-        {#if import.meta.env.DEV}
-            <pre>{$page.error?.stack}</pre>
-        {:else}
-            <p>
-                If this error persists when it seems like it really shouldn't, please contact me at 
-                <a href="mailto:jack@weilage.dev?subject=Recurring Error {$page.status}: {$page.error?.message}">jack@weilage.dev</a>
-                with a description of how to reproduce the error.
-            </p>
-        {/if}
+        <p>
+            If this error persists when it seems like it really shouldn't, please contact me at 
+            <a href="mailto:jack@weilage.dev?subject=Recurring Error {$page.status}: {$page.error?.message ?? 'Unknown'}">jack@weilage.dev</a>
+            with a description of how to reproduce the error.
+        </p>
     {:else}
         <h1>You're currently offline!</h1>
         <p>Wait for an internet connection, then reload this page.</p>
@@ -50,13 +46,6 @@
             border-bottom: 1px solid var(--theme-text);
         }
         p {
-            margin-bottom: 0.5rem;
-        }
-        pre {
-            font-size: 0.8rem;
-            white-space: pre-wrap;
-            word-break: break-all;
-    
             margin-bottom: 0.5rem;
         }
     }
