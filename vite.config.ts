6e7b09183220519a45081c8943d0a4cacd030b1c
Jack Weilage <jack@weilage.dev>
2022-10-24 09:28:53 -0700

    Format `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-// import image from 'vite-image'
 import { imagetools } from 'vite-imagetools'
-import { sveltekit } from '@sveltejs/kit/vite'
+import { sveltekit }  from '@sveltejs/kit/vite'
+
 import type { UserConfig } from 'vite'
 
 export default {
 	plugins: [ 
-		imagetools(),
-		// image(),
+		imagetools(), 
 		sveltekit() 
 	]
 } as UserConfig
