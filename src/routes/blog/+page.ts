0a9563bf177301bd794a5ea98e264ec38a8cd356
Jack Weilage <jack@weilage.dev>
2022-10-25 12:41:49 -0700

    Properly prerender

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,4 +1,1 @@
-// I assume this route isn't prerenderable because of this load function?
-export const prerender = false
-
 export const sitemap: import('!types').SitemapConfig = { enabled: true, changefreq: 'weekly', priority: 1 }
