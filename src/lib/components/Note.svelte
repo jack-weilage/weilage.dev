e37ce4d8a88ba07298586d4accfbca748edae4fc
Jack Weilage <jack@weilage.dev>
2022-10-28 19:39:57 -0700

    Create `<Note />` component

---
+++ src/lib/components/Note.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import Information from 'carbon-icons-svelte/lib/InformationFilled.svelte'
+</script>
+
+<div>
+    <Information aria-hidden="true" />
+    <p>
+        <strong>Note: </strong>
+        <slot />
+    </p>
+</div>
+
+<style lang="postcss">
+    div {
+        margin: 2rem 0;
+        padding: 1rem;
+
+        border-radius: 0.25rem;
+        border: 1px solid var(--color--border);
+        border-left: 5px solid var(--color--anchor);
+
+        background-color: var(--color--background-alt);
+
+
+        display: grid;
+        grid-template-columns: 3rem 1fr;
+        align-items: center;
+
+        & > :global(svg) {
+            color: var(--color--anchor);
+
+            width: 1.5rem;
+            height: 1.5rem;
+        }
+        & > p {
+            margin: 0;
+        }
+    }
+</style>
