990b07a3a887ee1d19b76557c2d6ae090f6049c7
Jack Weilage <jack@weilage.dev>
2022-10-31 19:41:05 -0700

    Don't prerender for now, fixes phantom pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,4 +2,5 @@ import { dev } from '$app/environment'
 
 //TODO: The back-forward cache seems to break when CSR is enabled and CSS is global.
 export const csr = false
-export const prerender = true
+//TODO: Pages seem to be ignored by vercel if prerendered. ????
+export const prerender = false
