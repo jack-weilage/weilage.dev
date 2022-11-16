7391f01be152b576b3a9489365127a0cca48e529
Jack Weilage <jack@weilage.dev>
2022-11-16 13:07:09 -0800

    Update deps, remove `vite-imagetools`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { imagetools } from 'vite-imagetools'
 import { sveltekit }  from '@sveltejs/kit/vite'
 
 export default {
 	plugins: [ 
-		imagetools(), 
 		sveltekit() 
 	]
 } as import('vite').UserConfig
