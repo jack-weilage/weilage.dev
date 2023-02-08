e14f413fb43092cc477a2996d1e8ead9f90cef5d
jack-weilage <jack@weilage.dev>
2023-02-08 14:40:19 -0800

    Fix formatting.

--- src/lib/posts/index.ts
+++ src/lib/posts/index.ts
@@ -1,14 +1,14 @@
+import type { Heading, PostData, ViteGlobImport } from '!types'
+
 import { browser } from '$app/environment'
 import { basename } from 'path'
 import { parse } from 'node-html-parser'
-import type { Heading, PostData, ViteGlobImport } from '!types'
 
 if (browser)
     throw new Error('Posts cannot be accessed from browser.')
 
-export const posts: PostData[] = Object.entries(import.meta.glob('./content/**/*.md', { eager: true  }) as ViteGlobImport)
+export const posts: PostData[] = Object.entries(import.meta.glob('./content/**/*.md', { eager: true }) as ViteGlobImport)
     .map(([ path, content ]) => {
-        
         // Create a slug
         const slug = basename(path)
             .replace('.md', '')
@@ -18,20 +18,20 @@ export const posts: PostData[] = Object.entries(import.meta.glob('./content/**/*
 
         // Get the text of the whole document (this won't include title or other metadata).
         const wordcount = $.structuredText
-                .split(' ')
-                .filter(Boolean)
-                .length
+            .split(' ')
+            .filter(Boolean)
+            .length
         
         const meta = content.metadata
         return {
             slug,
             draft:        !!meta.draft,
-            title:        meta.title       as string ?? 'Unknown Title',
-            description:  meta.description as string ?? 'Unknown Description',
-            date:         meta.date        as string ?? '1970-01-01',
+            title:        meta.title       as string    ?? 'Unknown Title',
+            description:  meta.description as string    ?? 'Unknown Description',
+            date:         meta.date        as string    ?? '1970-01-01',
             wordcount,
-            tags:         meta.tags     as string[]  ?? [],
-            headings:     meta.headings as Heading[] ?? []
+            tags:         meta.tags        as string[]  ?? [],
+            headings:     meta.headings    as Heading[] ?? []
         } as PostData
     })
     .sort((a, b) => +new Date(b.date) - +new Date(a.date))
