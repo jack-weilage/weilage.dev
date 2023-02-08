7a732a2c04497fa70326d7f474c47243c05caf4c
jack-weilage <jack@weilage.dev>
2023-02-08 14:03:19 -0800

    Remove unused import.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,3 @@
-import { dev } from '$app/environment'
-
 //TODO: The back-forward cache seems to break when CSR is enabled and CSS is global.
 export const csr = false
 //TODO: Pages seem to be ignored by vercel if prerendered. ????
