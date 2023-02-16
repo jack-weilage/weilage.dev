a54af9aae6109041c859a3adbc1ce00775603505
jack-weilage <jack@weilage.dev>
2023-02-15 18:35:23 -0800

    Properly use PostCSS plugins, switch to Node 18

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,23 @@ import mdsvex_config from './mdsvex.config.js'
 
 import preprocess from 'svelte-preprocess'
 
+import postcss_preset_env from 'postcss-preset-env'
+import postcss_autoprefixer from 'autoprefixer'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: [ '.svelte',  ...mdsvex_config.extensions ?? [] ],
     preprocess: [ 
         mdsvex(mdsvex_config), 
-        preprocess({ preserve: [ 'ld+json' ], postcss: true })
+        preprocess({
+            preserve: [ 'ld+json' ],
+            postcss: {
+                plugins: [
+                    postcss_preset_env({ stage: 1 }),
+                    postcss_autoprefixer()
+                ]
+            }
+        })
     ],
     kit: {
         adapter: adapter(),
