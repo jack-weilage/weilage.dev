3ce4af6871cc28f5cfba1a7b4519a6e0d92b4420
jack-weilage <jack@weilage.dev>
2023-02-20 23:03:06 -0800

    Simplify SEO manager, rename to `SEO.svelte`

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { Robots } from '!types'
+
+    export let title: string
+    export let description: string
+
+    export let robots: Robots = {}
+    export let json_ld: Record<string, unknown> | undefined = undefined
+
+    const robots_string = Object.keys(robots)
+        .filter(key => !!robots[key as keyof Robots])
+        .join()
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="description" content={description}>
+
+    {#if robots_string}
+        <meta name="robots" content={robots_string}>
+    {/if}
+    {#if json_ld}
+        {@html `<script type="application/ld+json">${JSON.stringify(json_ld)}</` + `script>`}
+    {/if}
+</svelte:head>
