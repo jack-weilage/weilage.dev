3f6d5b8aae36e00d4dfcad96bd48a9e41083b95a
jack-weilage <jack@weilage.dev>
2023-02-23 11:24:23 -0800

    Markdown should be treated as an asset.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import type { UserConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 
 export default {
-	plugins: [ sveltekit() ]
+	plugins: [ sveltekit() ],
+	assetsInclude: [ '**/*.md' ]
 } as UserConfig
