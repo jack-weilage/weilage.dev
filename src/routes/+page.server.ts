ecb41baa4ba0fa28a45ef44a8e28ef980eb55309
jack-weilage <jack@weilage.dev>
2023-02-25 13:14:46 -0800

    Upgrade homepage.

--- src/routes/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types'
-
-import { posts_no_drafts } from '$lib/posts'
-
-// Only return a few posts.
-export const load: PageServerLoad = () => ({ posts: posts_no_drafts.slice(0, 5) })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,233 +1,93 @@
-<!-- 
-TODO: Main title/text should be horizontally centered. Maybe vertical center? How to center around one element?
- -->
+<script context="module" lang="ts">
+    import type { SitemapConfig } from '!types'
+    export const _sitemap: SitemapConfig = {
+        enabled: true,
+        changefreq: 'weekly',
+        priority: 1
+    }
+</script>
 <script lang="ts">
-    import type { PageData } from './$types'
-
-    import SvEO from '!components/SvEO.svelte'
-    import Link from '!components/Link.svelte'
-    import PostPreview from '!components/PostPreview.svelte'
-
-    export let data: PageData
-
-    import { ChevronDown, Github, Figma, Mail} from 'lucide-svelte'
+    import { Mail, Github, Figma } from 'lucide-svelte'
+    import SEO from '!components/SEO.svelte'
 </script>
 
-<SvEO
-    title="Jack Weilage - Home"
-    description="The homepage of Jack Weilage, a web developer and designer."
-    author="Jack Weilage"
-    keywords={[ 'jack weilage', 'web developer', 'designer' ]}
+<SEO
+    title="Home - Jack Weilage" description="The homepage of Jack Weilage, a web developer and designer."
+    json_ld={{
+        '@context': 'http://schema.org/',
+        '@type': 'Person',
+        name: 'Jack Weilage',
+        gender: 'Mail',
+        sameAs: [
+            'https://github.com/jack-weilage',
+            'https://figma.com/@weilage'
+        ],
+        email: 'jack@weilage.dev',
+        image: '/images/pfp.jpg',
+        url: 'https://weilage.dev/'
+    }}
 />
-<svelte:head>
-    <script type="ld+json">
-        {
-            '@context': 'http://schema.org/',
-            '@type': 'Person',
-            'name': 'Jack Weilage',
-            'gender': 'Male',
-            'sameAs': [
-                'https://weilage.dev/',
-                'https://github.com/jack-weilage',
-                'https://figma.com/@weilage'
-            ],
-            'url': 'https://weilage.dev/'
-        }
-    </script>
-</svelte:head>
 
 <main id="main-content">
-    <section id="intro">
-        <div>
-            <h1>Jack Weilage</h1>
-            <p>
-                I'm a <Link href="https://github.com/jack-weilage">web developer</Link> <span class="spacer-sm" aria-hidden="true" />
-                and <Link href="https://figma.com/@weilage">designer</Link>, working <span class="spacer-xl" aria-hidden="true" />to
-                improve the web through performant code and accessible designs. Currently, I'm studying computer science and web
-                development, with a focus on performance, reactive layouts, and mobile compatibility. Outside of programming,
-                I love reading and learning more about technology and the world around me!
-            </p>
-            <ul>
-                <li>
-                    <Github size="18px" />
-                    <Link href="https://github.com/jack-weilage">
-                        Read my code!
-                    </Link>
-                </li>
-                <li>
-                    <Figma size="18px" />
-                    <Link href="https://figma.com/@weilage">
-                        Check out my designs!
-                    </Link>
-                </li>
-                <li>
-                    <Mail size="18px" />
-                    <Link href="mailto:jack@weilage.dev">
-                        Contact me!
-                    </Link>
-                </li>
-            </ul>
-        </div>
-        <Link href="#posts" class="next-section" title="Move to next section">
-            <ChevronDown size="16px" />
-        </Link>
-    </section>
-    <section id="posts">
-        <header>
-            <h2>Recently Posted</h2>
-        </header>
-        <main>
-            {#each data.posts as post}
-                <PostPreview {post} />
-            {/each}
-        </main>
-        <footer>
-            <Link href="/blog/">Browse all posts</Link>
-        </footer>
-    </section>
+    <h1>Jack Weilage</h1>
+    <p>Web Developer and Designer</p>
+    <ul>
+        <li>
+            <a target="_blank" rel="noopener noreferrer" href="mailto:jack@weilage.dev" title="Email me at jack@weilage.dev">
+                <Mail aria-hidden="true" size="20px" />
+            </a>
+        </li>
+        <li>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/jack-weilage" title="Read my code on GitHub">
+                <Github aria-hidden="true" size="20px" />
+            </a>
+        </li>
+        <li>
+            <a target="_blank" rel="noopener noreferrer" href="https://figma.com/@weilage" title="Check out my designs on Figma">
+                <Figma aria-hidden="true" size="20px" />
+            </a>
+        </li>
+    </ul>
 </main>
 
-<style lang="postcss" global>
-    @keyframes up-and-down {
-        to {
-            margin-bottom: -35%;
+<style lang="postcss">
+    main :global {
+        text-align: center;
+
+        height: 100vh;
+        height: 100dvh;
+
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+
+        & > h1 {
+            margin: 0;
+
+            font-size: clamp(2.25rem, 14vw, 2.75rem);
         }
-    }
-    a.next-section {
-        position: absolute;
-        bottom: 1rem;
+        & > p {
+            margin: 0;
 
-        line-height: 1;
-
-        border-radius: 50%;
-        width: 3rem;
-        height: 3rem;
-        background-color: var(--color--background-alt);
-
-        display: grid;
-        place-items: center;
-
-        /* Icon placed right in the middle, half-size */
-        & > svg {
-            width: 50%;
-            height: 50%;
-
-            animation: up-and-down 1s ease-in-out infinite alternate;
+            font-size: clamp(0.75em, 5vw, 1em);
+            color: var(--color--text-alt);
         }
-    }
-    section {
-        padding: 6rem;
+        & > ul {
+            list-style: none;
 
-        @media (width <= 800px) {
-            padding: 6rem 2rem;
-        }
-        @media (width <= 600px) {
-            padding: 4rem 1rem;
-        }
-    }
-    section#intro {
-        padding-top: 4rem;
-        margin-bottom: 2rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
 
-        @media (width <= 500px) {
-            padding-top: 1rem;
-        }
+            gap: 1.35rem;
 
-        display: grid;
-        place-items: center;
+            margin: 1rem 0 0;
+            padding: 0;
 
-        & div {
-            width: 75%;
-            @media (width <= 900px) {
-                width: 100%;
+            & > li > a {
+                display: flex;
+                place-items: center;
             }
-
-            & h1 {
-                margin: 0;
-                font-size: 5rem;
-                letter-spacing: -2px;
-                line-height: 0.8;
-            }
-            & p {
-                margin: 0.25em 0 0;
-
-                & a {
-                    font-weight: bold;
-                }
-            }
-            & ul {
-                list-style: none;
-                padding-left: 1rem;
-                margin-bottom: 0;
-
-                & li {
-                    padding: 0.25rem;
-                    font-weight: bold;
-
-                    display: flex;
-                    align-items: center;
-                    gap: 0.75rem;
-                }
-            }
-            & .spacer-xl {
-                display: inline-block;
-                width: 2.65rem;
-            }
-            & .spacer-sm {
-                display: none;
-                width: 4.5rem;
-            }
-
-            /* At 500px, the heading should collapse and a different spacer should be shown to break up different text. */
-            @media (width <= 500px) {
-                & h1 {
-                    width: min-content;
-                }
-                & p {
-                    margin: 0;
-                }
-                & .spacer-xl {
-                    display: none;
-                }
-                & .spacer-sm {
-                    display: inline-block;
-                }
-            }
-            /* 
-            At 335px, the heading should begin to shrink with screen size, making spacers obsolete. 
-            The heading should be placed higher so the letters don't overlap.
-            */
-            @media (width <= 335px) {
-                & h1 {
-                    font-size: 22vw;
-                    margin-bottom: 5vw;
-                }
-                & .spacer-sm {
-                    display: none;
-                }
-            }
-        }
-    }
-    section#posts {
-        & header {
-            text-align: center;
-            margin-bottom: 1.5rem;
-
-            & h2 {
-                font-size: 3rem;
-                margin: 0;
-            }
-        }
-        & footer a {
-            display: block;
-            
-            font-size: 1.75rem;
-            font-weight: bold;
-
-            margin-top: 1rem;
-            padding: 1.5rem;
-
-            text-align: center;
         }
     }
 </style>
