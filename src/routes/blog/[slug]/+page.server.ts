51f74061ebf4c85aeab83c2d129795c2492f12b1
jack-weilage <jack@weilage.dev>
2023-03-16 12:07:06 -0700

    Only use one request for draft posts.

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,16 +1,18 @@
 import { database } from '$lib/database.server'
+import { dev } from '$app/environment'
 import { error } from '@sveltejs/kit'
 
 export async function load({ params })
 {
     const post = await database.from('posts')
         .select('title,description,content')
-        .eq('draft', false)
         .eq('slug', params.slug)
+        // Either draft === false or draft === dev.
+        .or(`draft.eq.false,draft.eq.${dev}`)
         .limit(1)
         .maybeSingle()
 
-    if (!post?.data || post.error)
+    if (!post.data || post.error)
         throw error(404, 'Post Not Found')
 
     return {
