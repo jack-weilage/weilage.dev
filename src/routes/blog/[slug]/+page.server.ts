a27ccb3d9a690a50f7205f28218beebda35e5177
jack-weilage <jack@weilage.dev>
2023-03-16 09:39:12 -0700

    Fail if no data is returned.

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -10,7 +10,7 @@ export async function load({ params })
         .limit(1)
         .maybeSingle()
 
-    if (!post || post.error)
+    if (!post?.data || post.error)
         throw error(404, 'Post Not Found')
 
     return {
