91cf39a2d02dfb0f026dfbe78fee01a3e83b5738
Jack Weilage <jack@weilage.dev>
2024-04-06 19:58:42 -0700

    fix(posts): Remove param matcher bc it runs on client + server

--- src/params/slugged.ts
+++
@@ -1,6 +0,0 @@
-import { posts } from '$lib/posts'
-import type { ParamMatcher } from '@sveltejs/kit'
-
-export const match: ParamMatcher = (slug) => {
-	return posts.some((post) => post.slug === slug)
-}
