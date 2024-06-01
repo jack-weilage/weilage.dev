735a416c9161009ad960f4d143a59e575d18bacd
Jack Weilage <jack@weilage.dev>
2024-05-31 19:11:50 -0700

    fix(resume): Remove unnecessary margin above headings

--- src/routes/resume/Section.svelte
+++ src/routes/resume/Section.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section>
-	<h2 class="mt-4 text-center text-2xl font-bold">{title}</h2>
+	<h2 class="text-center text-2xl font-bold">{title}</h2>
 	<div class="flex flex-col gap-y-8">
 		<slot />
 	</div>
